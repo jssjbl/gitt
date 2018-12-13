f88b07234fde4c99b43d5a5c58b8bd4e079da108
yyf123 <1622163535@qq,com>
2018-12-13 17:10:56 +0800

    hot_fix

--- src/main/java/com/sunysrd/Proxy/test.java
+++ src/main/java/com/sunysrd/Proxy/test.java
@@ -4,5 +4,6 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("right....right");
 		System.out.println("left.........right");
+		System.out.println("hot_fix");
 	}
 }
