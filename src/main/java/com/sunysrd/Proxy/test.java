8835b8001ffd41e0a4594ea429a44d93c0c1d57e
yyf123 <1622163535@qq,com>
2018-12-13 17:08:16 +0800

    sdqqqqqqqqqqqq

--- src/main/java/com/sunysrd/Proxy/test.java
+++ src/main/java/com/sunysrd/Proxy/test.java
@@ -3,6 +3,6 @@ package com.sunysrd.Proxy;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("right....right");
-		System.out.println("left....right");
+		System.out.println("left.........right");
 	}
 }
